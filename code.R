856fe5880dc54c2fb7ad75adf17fa7bb07c955d8
HaiSalami186 <hai186nguyen2003@gmail.com>
2024-11-04 22:00:23 -0500

    Update code.R

---
+++ code.R
@@ -0,0 +1,5 @@
+#install libraries
+library(dplyr)
+library(tidyr)
+#read data
+df = read.csv("D:\\York U Files\\F2025Courses\\ADMS4370\\Project\\mobile-user-behaviour-analysis\\data\\user_behavior_dataset.csv")
